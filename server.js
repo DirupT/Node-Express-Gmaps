6cc48f4d2c0e9b6e37d7254ea9f6047e95428636
DirupT <iamcooled@gmail.com>
2018-07-25 16:55:35 -0500

    added searchUrl / searchQuery variables

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const config = require('./config.js');
+const fetch = require('node-fetch');
 
 const server = express();
+
 const API_KEY = config.key;
+const SEARCH_PLACE_URL = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=';
 
 server.get('/', (req, res) => {
-    res.send(config.key);
+    res.send('Home');
+})
+
+server.get('/place', (req, res) => {
+    let searchQuery = req.query.search_query.split(' ').join('+');
+    let searchUrl = SEARCH_PLACE_URL + searchQuery + '&key=' + API_KEY;
+    console.log(searchUrl);
+    res.send('Place');
 })
 
 server.listen(8001, () => console.log('Gmaps api running...'));
